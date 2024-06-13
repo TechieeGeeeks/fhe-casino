867934db483b6c730d8d869633f7234ddd852744
Vivek Sahu <viveksahu1762@gmail.com>
2024-06-13 22:43:20 +0530

    Added winnig condition as well

--- client/src/app/games/coin-flip/page.js
+++ client/src/app/games/coin-flip/page.js
@@ -1,7 +1,8 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import CoinFlip from "@/modules/coin-flip";
 import CoinFlipForm from "@/modules/coin-flip/coinFlipInputForm";
+import { toast } from "@/components/ui/use-toast";
 import { confetti } from "@/utils/confetii";
 import {
   Accordion,
@@ -9,10 +10,9 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import React, { useEffect, useState } from "react";
-import { toast } from "@/components/ui/use-toast";
+import CoinFlip from "@/modules/coin-flip";
 
-const Page = () => {
+const Index = () => {
   const [isFlipping, setIsFlipping] = useState(false);
   const [result, setResult] = useState("Coin");
   const [isBtnDisbled, setIsBtnDisbled] = useState(false);
@@ -22,11 +22,29 @@ const Page = () => {
   const [maxPayout, setMaxPayout] = useState();
   const [stopOnLoss, setStopOnLoss] = useState();
   const [takeprofit, setTakeprofit] = useState();
+  const [userChoiced, setUserChoiced] = useState();
+
+  useEffect(() => {
+    if (userChoiced?.toLowerCase() === result.toLowerCase()) {
+      console.log(userChoiced, result)
+      confetti();
+      toast({
+        variant: "white",
+        title: "🎉🎉🎊Wohoooo!, You have won the toss",
+      });
+    }
+  }, [result, userChoiced]);
 
   const startFlipping = () => {
     if (!wager) {
       toast({
         title: "Please, add valid wager",
+      });
+      return;
+    }
+    if (!userChoiced) {
+      toast({
+        title: "Please, Choose heads or tails!",
       });
       return;
     }
@@ -48,11 +66,12 @@ const Page = () => {
     setIsFlipping(false);
     setResult(Math.random() < 0.5 ? "Heads" : "Tails");
   };
+
   return (
     <div>
-      <main className="relative flex flex-col items-center justify-center bg-white px-5 py-[150px] text-center font-bold bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] border-b-4 border-x-0 border-t-0 border-black">
-        <div className="grid gap-24 grid-cols-2">
-          <div className="flex flex-col gap-4">
+      <main className="relative flex flex-col items-center justify-center bg-white px-5 py-[150px] text-center font-bold bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+        <div className="grid gap-4 grid-cols-2">
+          <div className="max-w-[70%] flex flex-col gap-4">
             <CoinFlipForm
               id={"wager"}
               label={"Wager"}
@@ -119,15 +138,16 @@ const Page = () => {
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
-            <Button
-              onClick={startFlipping}
-              className="bg-main max-w-sm"
-              disabled={isBtnDisbled}>
+            <Button onClick={startFlipping} className="bg-main">
               Play
             </Button>
           </div>
-          <div className="md:flex hidden relative w-[300px] h-auto">
-            <CoinFlip isFlipping={isFlipping} result={result} />
+          <div className="md:flex hidden relative">
+            <CoinFlip
+              isFlipping={isFlipping}
+              result={result}
+              setUserChoiced={setUserChoiced}
+            />
           </div>
         </div>
       </main>
@@ -135,4 +155,144 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default Index;
+
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import CoinFlip from "@/modules/coin-flip";
+// import CoinFlipForm from "@/modules/coin-flip/coinFlipInputForm";
+// import { confetti } from "@/utils/confetii";
+// import {
+//   Accordion,
+//   AccordionContent,
+//   AccordionItem,
+//   AccordionTrigger,
+// } from "@/components/ui/accordion";
+// import React, { useEffect, useState } from "react";
+// import { toast } from "@/components/ui/use-toast";
+
+// const Page = () => {
+//   const [isFlipping, setIsFlipping] = useState(false);
+//   const [result, setResult] = useState("Coin");
+//   const [isBtnDisbled, setIsBtnDisbled] = useState(false);
+//   const [wager, setWager] = useState();
+//   const [bet, setBet] = useState(1);
+//   const [totalwager, setTotalwager] = useState();
+//   const [maxPayout, setMaxPayout] = useState();
+//   const [stopOnLoss, setStopOnLoss] = useState();
+//   const [takeprofit, setTakeprofit] = useState();
+
+//   const startFlipping = () => {
+//     if (!wager) {
+//       toast({
+//         title: "Please, add valid wager",
+//       });
+//       return;
+//     }
+//     setIsFlipping(true);
+//     setIsBtnDisbled(true);
+//     setResult("🪙");
+//   };
+
+//   useEffect(() => {
+//     if (isFlipping) {
+//       setTimeout(() => {
+//         stopFlipping();
+//       }, 5000);
+//     }
+//   }, [isFlipping]);
+
+//   const stopFlipping = () => {
+//     setIsBtnDisbled(false);
+//     setIsFlipping(false);
+//     setResult(Math.random() < 0.5 ? "Heads" : "Tails");
+//   };
+//   return (
+//     <div>
+//       <main className="relative flex flex-col w-full items-center justify-center bg-white px-5 py-[150px] text-center font-bold bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] border-b-4 border-x-0 border-t-0 border-black">
+//         <div className="flex gap-24">
+//           <div className="flex flex-col gap-4">
+//             <CoinFlipForm
+//               id={"wager"}
+//               label={"Wager"}
+//               onChange={(e) => setWager(e.target.value)}
+//               placeholder={"Choose Wager"}
+//               type={"number"}
+//               value={wager}
+//             />
+//             <CoinFlipForm
+//               id={"bets"}
+//               label={"Bets"}
+//               onChange={(e) => setBet(e.target.value)}
+//               placeholder={""}
+//               type={"number"}
+//               value={bet}
+//             />
+
+//             <div className="w-full grid grid-cols-2 gap-4">
+//               <CoinFlipForm
+//                 id={"totalwager"}
+//                 label={"Total Wager"}
+//                 onChange={(e) => setTotalwager(e.target.value)}
+//                 placeholder={"-"}
+//                 disabled={true}
+//                 type={"number"}
+//                 value={totalwager}
+//                 className={'w-full'}
+//               />{" "}
+//               <CoinFlipForm
+//                 id={"maxpayout"}
+//                 label={"Max Payout"}
+//                 onChange={(e) => setMaxPayout(e.target.value)}
+//                 placeholder={"-"}
+//                 disabled={true}
+//                 type={"number"}
+//                 value={maxPayout}
+//               />
+//             </div>
+
+//             <Accordion
+//               className="w-full bg-white border-none shadow-none"
+//               type="single"
+//               collapsible>
+//               <AccordionItem className="max-w-full" value="item-1">
+//                 <AccordionTrigger className="bg-transparent">
+//                   Advanced
+//                 </AccordionTrigger>
+//                 <AccordionContent className="grid grid-cols-2 gap-4">
+//                   <CoinFlipForm
+//                     id={"stoponloss"}
+//                     label={"Stop on Loss"}
+//                     onChange={(e) => setStopOnLoss(e.target.value)}
+//                     placeholder={"-"}
+//                     type={"number"}
+//                     value={stopOnLoss}
+//                   />{" "}
+//                   <CoinFlipForm
+//                     id={"takeprofit"}
+//                     label={"Take Profit"}
+//                     onChange={(e) => setTakeprofit(e.target.value)}
+//                     placeholder={"-"}
+//                     type={"number"}
+//                     value={takeprofit}
+//                   />
+//                 </AccordionContent>
+//               </AccordionItem>
+//             </Accordion>
+//             <Button
+//               onClick={startFlipping}
+//               className="bg-main max-w-sm"
+//               disabled={isBtnDisbled}>
+//               Play
+//             </Button>
+//           </div>
+//           <div className="md:flex hidden relative w-[300px] h-auto">
+//             <CoinFlip isFlipping={isFlipping} result={result} />
+//           </div>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default Page;
--- client/src/modules/coin-flip/index.js
+++ client/src/modules/coin-flip/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
-const CoinFlip = ({ isFlipping, result }) => {
+const CoinFlip = ({ isFlipping, result, setUserChoiced }) => {
   const controls = useAnimation();
 
   useEffect(() => {
@@ -37,12 +39,28 @@ const CoinFlip = ({ isFlipping, result }) => {
   }, [isFlipping, controls]);
 
   return (
-    <div className="flex flex-col items-center justify-center absolute mt-20">
+    <div className="flex flex-col items-center justify-between h-full">
       <motion.div
         animate={controls}
         className="bg-main border-2 border-black shadow-base w-52 aspect-square rounded-full flex items-center justify-center">
         {result}
       </motion.div>
+
+      <div>
+        <RadioGroup
+          // defaultValue="comfortable"
+          className="flex"
+          onValueChange={(e) => setUserChoiced(e)}>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="heads" id="r1" />
+            <Label htmlFor="r1">Heads</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="tails" id="r2" />
+            <Label htmlFor="r2">Tails</Label>
+          </div>
+        </RadioGroup>
+      </div>
     </div>
   );
 };
