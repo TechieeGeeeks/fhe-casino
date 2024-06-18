041d8f9c5b1c737a0278a5008e9bbb7d0043e2c1
Vivek Sahu <viveksahu1762@gmail.com>
2024-06-18 23:47:36 +0530

    Build Done

--- client/src/app/not-found.js
+++ client/src/app/not-found.js
@@ -1,17 +1,19 @@
 "use client";
 import React from "react";
-import "@dotlottie/player-component";
+import Lottie from "lottie-react";
+import notFoundAnimation from "@/assets/404nf.json";
 
 const NotFoundPage = () => {
   return (
     <div className="grid place-items-center min-h-[120vh] p-20">
       <div className="w-[35%]">
-        <dotlottie-player
+        <Lottie animationData={notFoundAnimation} loop={true} />
+        {/* <dotlottie-player
           autoplay
           loop
           playMode="normal"
           src="/404nf.json"
-        ></dotlottie-player>
+        ></dotlottie-player> */}
         {/* <Lottie animationData={'/404nf.json'} loop={true} /> */}
       </div>
     </div>
